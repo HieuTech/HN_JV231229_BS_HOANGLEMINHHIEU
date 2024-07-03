d0bb09cf8a0f27d0e48e3c27f512defb624aa214
Your Name <you@example.com>
2024-07-03 16:54:57 +0700

    first

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,75 +1,130 @@
-import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
-import { Modal } from 'antd';
-import { useState } from 'react'
-import Footer from './Footer';
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Modal } from "antd";
+import { useState } from "react";
+import Footer from "./Footer";
 
 export default function Content(prop) {
-    const { data, saveData ,task, setTask} = prop;
-    const [openDelete, setOpenDelete] = useState(false);
+  const { data, saveData, task, setTask } = prop;
+  const [openDelete, setOpenDelete] = useState(false);
+
+  const [openEdit, setOpenEdit] = useState(false);
+
+  const [taskUpdate, setTaskUpdate] = useState(null);
+
+  const [taskUpdateValue, setTaskUpdateValue] = useState(null);
+  const [taskDeleteValue, setTaskDeleteValue] = useState(null);
+
+  //CHECKED
+
+  const handleChecked = (taskCheck) => {
+    const taskChangeStatusIndex = data.map((task) => {
+      if (task.id === taskCheck.id) {
+        task.status = !task.status;
+      }
+      return task;
+    });
+    saveData(taskChangeStatusIndex);
+  };
+
+  //EDIT
+
+  const handleOnchangeEditTask = (e) => {
+    const { name, value } = e.target;
+   
+    const newUpdate = {
+      id: taskUpdate.id,
+      taskName: value,
+      status: taskUpdate.status,
+    };
+
+    setTaskUpdate(newUpdate);
+    setTaskUpdateValue(value);
+
+    setTask({ ...task, [name]: value });
+  };
+
+  const handleOpenEdit = (taskUpdate) => {
+    setTaskUpdate(taskUpdate);
+    setTaskUpdateValue(taskUpdate.taskName);
+
+    setOpenEdit(true);
+  };
+
+  const handleCloseEdit = () => {
+    setOpenEdit(false);
+  };
+
+  const handleConfirmEdit = (taskEdit) => {
+    console.log("taskEdit", taskEdit);
+
     
-    const [openEdit, setOpenEdit] = useState(false);
+    const newArr = data.map((t) => {
+      if (t.id === taskEdit.id) {
+        t.id = taskEdit.id;
+        t.taskName = taskEdit.taskName;
+        t.status = taskEdit.status;
+      }
+      return t;
+    });
 
-    const [taskUpdateValue, setTaskUpdateValue] = useState("");
-     const handleDelete = (taskDelete) => {
-         console.log("taskDelete", taskDelete);
-       const newArr = data.filter((task) => taskDelete.id !== task.id);
-       console.log("newArr", newArr);
-       saveData(newArr);
-       handleCloseDelete();
-     };
+    saveData(newArr);
 
-     const handleCloseDelete = () => {
-       setOpenDelete(false);
-     };
+    handleCloseEdit();
+  };
 
-     const handleOnchangeEditTask = (e) => {
-       const { name, value } = e.target;
+  //DELETE
 
-       setTaskUpdateValue(value);
-       setTask({ ...task, [name]: value });
-     };
+  const handleDelete = (taskDelete) => {
+    const newArr = data.filter((task) => taskDelete.id !== task.id);
+    console.log("newArr", newArr);
 
-       const handleChecked = (taskCheck) => {
-         const taskChangeStatusIndex = data.map((task) => {
-           if (task.id === taskCheck.id) {
-             task.status = !task.status;
-           }
-           return task;
-         });
-         saveData(taskChangeStatusIndex);
-       };
+    saveData(newArr);
+    handleCloseDelete();
+  };
 
-     //Edit
-     const handleOpenEdit = (task) => {
-       setTaskUpdateValue(task.taskName);
-       setOpenEdit(true);
-     };
-     const handleCloseEdit = () => {
-       setOpenEdit(false);
-     };
-     const handleConfirmEdit = (taskEdit) => {
-       const newArr = data.map((t) => {
-         if (t.id === taskEdit.id) {
-           t.id = taskEdit.id;
-           t.taskName = task.taskName;
-           t.status = taskEdit.status;
-         }
-         return t;
-       });
-       console.log("newArr", newArr);
+  const handleCloseDelete = () => {
+    setOpenDelete(false);
+  };
 
-       saveData(newArr);
-
-       handleCloseEdit();
-     };
-
-  const handleOpenDelete = () => {
+  const handleOpenDelete = (taskDelete) => {
+    setTaskDeleteValue(taskDelete);
     setOpenDelete(true);
   };
 
-
   return (
     <>
+      <Modal
+        title="Cập nhật công việc"
+        open={openEdit}
+        onOk={() => {
+          handleConfirmEdit(taskUpdate);
+        }}
+        onCancel={handleCloseEdit}
+        okText="Đồng ý"
+        cancelText="Hủy"
+      >
+        <p>Tên công việc</p>
+        <input
+          name="taskName"
+          value={taskUpdateValue}
+          className="w-full rounded-sm bg-orange-50 "
+          onChange={handleOnchangeEditTask}
+        ></input>
+      </Modal>
+
+      <Modal
+        title="Confirm Delete"
+        open={openDelete}
+        onOk={() => {
+          handleDelete(taskDeleteValue);
+        }}
+        onCancel={handleCloseDelete}
+        okText="Đồng ý"
+        cancelText="Hủy"
+      >
+        <p>Do you want to delete {task.taskName} task?</p>
+      </Modal>
+
       {data.length === 0 ? (
         <img
           src="https://img.freepik.com/premium-vector/no-data-concept-illustration_86047-488.jpg"
@@ -105,46 +160,18 @@ export default function Content(prop) {
                       handleOpenEdit(task);
                     }}
                   />
-                  <Modal
-                    title="Cập nhật công việc"
-                    open={openEdit}
-                    onOk={() => {
-                      handleConfirmEdit(task);
-                    }}
-                    onCancel={handleCloseEdit}
-                    okText="Đồng ý"
-                    cancelText="Hủy"
-                  >
-                    <p>Tên công việc</p>
-                    <input
-                      name="taskName"
-                      value={taskUpdateValue}
-                      className="w-full rounded-sm bg-orange-50 "
-                      onChange={handleOnchangeEditTask}
-                    ></input>
-                  </Modal>
+
                   <DeleteOutlined
                     className="text-red-600 cursor-pointer"
-                    onClick={handleOpenDelete}
-                  />
-                  <Modal
-                    title="Confirm Delete"
-                    open={openDelete}
-                    onOk={() => {
-                      handleDelete(task);
+                    onClick={() => {
+                      handleOpenDelete(task);
                     }}
-                    
-                    onCancel={handleCloseDelete}
-                    okText="Đồng ý"
-                    cancelText="Hủy"
-                  >
-                    <p>Do you want to delete {task.taskName} task?</p>
-                  </Modal>
+                  />
                 </div>
               </li>
             ))}
           </ul>
-          <Footer data={data}/>
+          <Footer data={data} />
         </div>
       )}
     </>
